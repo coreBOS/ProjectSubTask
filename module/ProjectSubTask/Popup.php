4979a6d758d795c7b588a0d2d5d64f2eb8ace10e
Joe Bordes <joe@tsolucio.com>
2020-01-17 16:22:22 +0100

    correct structure, sync with vtlib and add language files

--- module/ProjectSubTask/Popup.php
+++ module/ProjectSubTask/Popup.php
@@ -7,8 +7,5 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  ************************************************************************************/
-if (isset($_REQUEST['return_module']) && $_REQUEST['return_module']=='Project' && $_REQUEST['popuptype']=='detailview' && $_REQUEST['form']=='EditView') {
-	$where="vtiger_projecttask.projectid in ('',NULL)";
-}
 require_once 'Popup.php';
 ?>
---
+++ module/ProjectSubTask/language/es_es.lang.php
@@ -0,0 +1,55 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * ***********************************************************
+*  Module       : ProjectSubTask
+*  Language     : ES Spanish
+*  Version      : vt5.4.0
+*  Created Date : 2020-01-17
+*  Author       : JPL TSolucio, S.L.
+*  License      : MPL 1.1
+ ************************************************************************************/
+$mod_strings = array(
+	'LBL_MODULE_NAME'=>'Subtareas',
+	'LBL_MODULE_TITLE'=>'Subtareas',
+	'LBL_NEW_PROJECTTASK'=>'Nueva Subtarea',
+	'LBL_PROJECTTASKS'=>'Subtareas',
+	'LBL_PROJECTTASK'=>'Subtarea',
+	'Project Task ID'=>'ID Subtarea',
+	'projectasks' => 'subtareas',
+	'ProjectTask'=>'Subtareas',
+	'SINGLE_ProjectTask' => 'Subtarea',
+	'LBL_CUSTOM_INFORMATION' => 'Información Personalizada',
+	'LBL_PROJECT_TASK_INFORMATION' => 'Información Subtarea',
+	'LBL_DESCRIPTION_INFORMATION' => 'Descripción',
+	'Assigned To' => 'Asignado a',
+	'Priority' => 'Prioridad',
+	'Progress' => 'Progreso',
+	'Open' => 'Abierta',
+	'Cancelled'=>'Cancelada',
+	'Type'=>'Tipo',
+	'Worked Hours'=>'Horas Trabajadas',
+	'Start Date'=>'Fecha inicio',
+	'End Date'=>'Fecha fin',
+	'Related to' => 'Actividad',
+	'administrative' => 'administrativo',
+	'operative' => 'operativo',
+	'other' => 'otro',
+	'low' => 'baja',
+	'normal' => 'normal',
+	'high' => 'alta',
+	//added for related lists
+	'Project Task Name' => 'Nombre Subtarea',
+	'Project Task No' => 'Nº Subtarea',
+	'Project Task Number' => 'Código Subtarea',
+	'ProjectTask ID'=>'Id Subtarea',
+	'Add Note' => 'Añadir Documento',
+	'--none--' => '--ninguno--',
+	'Project Task' => 'Actividad',
+);
+?>

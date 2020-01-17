7ce24d3ebe0b43e74f577796690aa0a7136becc3
shibe2018 <mohamed.staffs2018@gmail.com>
2020-01-17 16:33:28 +0300

    feat(ProjectSubTask) adding a new module

---
+++ module/ProjectSubTask/language/en_us.lang.php
@@ -0,0 +1,47 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+$mod_strings = array(
+	'LBL_MODULE_NAME'=>'Project Sub Tasks',
+	'LBL_MODULE_TITLE'=>'Project Sub Tasks',
+	'LBL_NEW_PROJECTSUBTASK'=>'New Project Sub Task',
+	'LBL_PROJECTSUBTASKS'=>'Project Sub Tasks',
+	'LBL_PROJECTSUBTASK'=>'Project Sub Task',
+	'Project Sub Task ID'=>'Project Sub Task ID',
+	'projectSubtasks' => 'projectsubtasks',
+	//added this to translate the module name in the main app menu
+	'ProjectSubTask'=>'Project Sub Tasks',
+	'SINGLE_ProjectSubTask' => 'Project Sub Task',
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_PROJECT_SUB_TASK_INFORMATION' => 'Project Sub Task Information',
+	'LBL_DESCRIPTION_INFORMATION' => 'Description Information',
+	'Assigned To' => 'Assigned To',
+	'Priority' => 'Priority',
+	'Progress' => 'Progress',
+	'Open'=>'Open',
+	'Cancelled'=>'Cancelled',
+	'Type'=>'Type',
+	'Worked Hours'=>'Worked Hours',
+	'Start Date'=>'Start Date',
+	'End Date'=>'End Date',
+	'Related to' => 'Project Task',
+	'administrative' => 'administrative',
+	'operative' => 'operative',
+	'other' => 'other',
+	'low' => 'low',
+	'normal' => 'normal',
+	'high' => 'high',
+	//added for related lists
+	'Project Sub Task Name' => 'Sub Task Name',
+	'Project Sub Task No' => 'Sub Task No',
+	'Project Sub Task Number' => 'Sub Task Code',
+	'ProjectSubTask ID'=>'ProjectSubTask ID',
+	'Project Task' => 'Project Task',
+);
+?>
